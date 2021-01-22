d9e6142548c9e9edbe6060e7c3d7207b823747de
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-01-21 23:30:40 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,17 +21,7 @@ sudo apt -y --fix-broken install
 #Installing Dependencies
 sudo apt -y install libmbedtls-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libcurl4-openssl-dev libfontconfig1-dev libfreetype6-dev libgl1-mesa-dev libjack-jackd2-dev libjansson-dev libluajit-5.1-dev libpulse-dev libqt5x11extras5-dev libspeexdsp-dev libswresample-dev libswscale-dev libudev-dev libv4l-dev libvlc-dev libx11-dev libx11-xcb1 libx11-xcb-dev libxcb-xinput0 libxcb-xinput-dev libxcb-randr0 libxcb-randr0-dev libxcb-xfixes0 libxcb-xfixes0-dev libx264-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcomposite-dev libxinerama-dev pkg-config python3-dev qtbase5-dev libqt5svg5-dev swig
 
-echo '''
-[Desktop Entry]
-Version=1.0
-Name=OBS Studio
-GenericName=Streaming/Recording Software
-Comment=Free and Open Source Streaming/Recording Software
-Exec=MESA_GL_VERSION_OVERRIDE=3.3 obs
-Icon=obs
-Terminal=false
-Type=Application
-Categories=AudioVideo;Recorder;
-StartupNotify=true
-StartupWMClass=obs
-''' >> ~/.local/share/applications/obs.desktop
+#Removing Desktop File Created by obs
+sudo rm /usr/sh
+
+sudo sed -i '/Exec/c\Exec=MESA_GL_VERSION_OVERRIDE=3.3 obs' /usr/share/applications/com.obsproject.Studio.desktop
