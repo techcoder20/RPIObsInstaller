b7e4722747e3c22820a86805d2e939d4ebe465db
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-01-21 22:35:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+#Removing Deb files if already downloaded
 rm libfdk-aac2_2.0.1-1_armhf.deb libfdk-aac-dev_2.0.1-1_armhf.deb
 
+#Downloading deb files
 wget http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/libfdk-aac2_2.0.1-1_armhf.deb
 wget http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/libfdk-aac-dev_2.0.1-1_armhf.deb
 
+#Installing Deb Files
 sudo dpkg -i libfdk-aac2_2.0.1-1_armhf.deb
 sudo dpkg -i libfdk-aac-dev_2.0.1-1_armhf.deb
 
+#Removing deb files after installation
 rm libfdk-aac2_2.0.1-1_armhf.deb libfdk-aac-dev_2.0.1-1_armhf.deb
 
+#Installing Obs
 sudo dpkg -i ~/RPIObsInstaller/obs_armhf.deb
 
 echo '''
@@ -18,7 +23,7 @@ Version=1.0
 Name=OBS Studio
 GenericName=Streaming/Recording Software
 Comment=Free and Open Source Streaming/Recording Software
-Exec=obs
+Exec=MESA_GL_VERSION_OVERRIDE=3.3 obs
 Icon=obs
 Terminal=false
 Type=Application
