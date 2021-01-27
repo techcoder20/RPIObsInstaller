3531d2421359e95472ef27bd968ae9c12fa810a6
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-01-27 03:46:58 -0600

    Update install-64.sh

--- install-64.sh
+++ install-64.sh
@@ -12,7 +12,7 @@ sudo apt update
 sudo apt -y install libmbedtls-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libcurl4-openssl-dev libfontconfig1-dev libfreetype6-dev libgl1-mesa-dev libjack-jackd2-dev libjansson-dev libluajit-5.1-dev libpulse-dev libqt5x11extras5-dev libspeexdsp-dev libswresample-dev libswscale-dev libudev-dev libv4l-dev libvlc-dev libx11-dev libx11-xcb1 libx11-xcb-dev libxcb-xinput0 libxcb-xinput-dev libxcb-randr0 libxcb-randr0-dev libxcb-xfixes0 libxcb-xfixes0-dev libx264-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcomposite-dev libxinerama-dev pkg-config python3-dev qtbase5-dev libqt5svg5-dev swig || error "Unable to install dependencies :("
 
 #Removing Deb files if already downloaded
-sudo rm -f ~/libfdk-aac2_2.0.1-1_armhf.deb ~/libfdk-aac-dev_2.0.1-1_armhf.deb ~/obs_armhf.deb
+sudo rm -f ~/libfdk-aac2_2.0.1-1_arm64.deb ~/libfdk-aac-dev_2.0.1-1_arm64.deb ~/obs_arm64.deb
 
 #Downloading deb files
 wget http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/libfdk-aac2_2.0.1-1_arm64.deb -O ~/libfdk-aac2_2.0.1-1_armhf.deb || error "Failed to download libfdk-aac2_2.0.1-1_armhf.deb"
