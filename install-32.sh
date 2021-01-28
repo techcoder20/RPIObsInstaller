e983fc0c740c797b76508dd1cc3b5f1bdc5529bc
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-01-27 18:54:07 -0600

    Update install-32.sh

--- install-32.sh
+++ install-32.sh
@@ -12,7 +12,7 @@ sudo apt update
 sudo apt -y install libmbedtls-dev libasound2-dev libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libcurl4-openssl-dev libfontconfig1-dev libfreetype6-dev libgl1-mesa-dev libjack-jackd2-dev libjansson-dev libluajit-5.1-dev libpulse-dev libqt5x11extras5-dev libspeexdsp-dev libswresample-dev libswscale-dev libudev-dev libv4l-dev libvlc-dev libx11-dev libx11-xcb1 libx11-xcb-dev libxcb-xinput0 libxcb-xinput-dev libxcb-randr0 libxcb-randr0-dev libxcb-xfixes0 libxcb-xfixes0-dev libx264-dev libxcb-shm0-dev libxcb-xinerama0-dev libxcomposite-dev libxinerama-dev pkg-config python3-dev qtbase5-dev libqt5svg5-dev swig || error "Unable to install dependencies :("
 
 #Removing Deb files if already downloaded
-sudo rm -f ~/libfdk-aac2_2.0.1-1_armhf.deb ~/libfdk-aac-dev_2.0.1-1_armhf.deb ~/obs_armhf.deb
+rm -f ~/libfdk-aac2_2.0.1-1_armhf.deb ~/libfdk-aac-dev_2.0.1-1_armhf.deb ~/obs_armhf.deb
 
 #Downloading deb files
 wget http://ftp.debian.org/debian/pool/non-free/f/fdk-aac/libfdk-aac2_2.0.1-1_armhf.deb -O ~/libfdk-aac2_2.0.1-1_armhf.deb || error "Failed to download libfdk-aac2_2.0.1-1_armhf.deb"
@@ -27,23 +27,7 @@ sudo apt -y install --fix-missing ~/obs_armhf.deb || error "Failed to install ~/
 #Removing deb files after installation
 sudo rm -f ~/libfdk-aac2_2.0.1-1_armhf.deb ~/libfdk-aac-dev_2.0.1-1_armhf.deb ~/obs_armhf.deb
 
-#Removing existing desktop file
-sudo rm /usr/share/applications/com.obsproject.Studio.desktop
-
-#Creating run file for obs
-echo "#!/bin/bash
-MESA_GL_VERSION_OVERRIDE=3.3 obs" >> ~/.obsrun.sh || error "Failed to make ~/.obsrun.sh"
-sudo chmod +x ~/.obsrun.sh
-
-#Creating desktop file
-echo "[Desktop Entry]
-Version=1.0
-Name=OBS Studio
-Comment=Free and Open Source Streaming/Recording Software
-Exec=/home/$USER/.obsrun.sh
-Icon=com.obsproject.Studio
-Terminal=false
-Type=Application
-Categories=AudioVideo;Recorder;
-StartupNotify=true
-StartupWMClass=obs" >> ~/.local/share/applications/com.obsproject.Studio.desktop || error "Failed to make desktop file for obs"
+sudo mv /usr/bin/obs /usr/bin/obs-exec
+echo '#!/bin/bash
+MESA_GL_VERSION_OVERRIDE=3.3 /usr/bin/obs-exec' | sudo tee /usr/bin/obs >/dev/null
+sudo chmod +x /usr/bin/obs
